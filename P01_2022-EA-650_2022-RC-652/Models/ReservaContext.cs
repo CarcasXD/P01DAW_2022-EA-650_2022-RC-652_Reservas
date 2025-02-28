6fa8218190be1658b8361591d261c1383c178fc1
CarcasXD <carlossalesianoprro@gmail.com>
2025-02-27 18:34:37 -0600

    Contexto Cambiado

--- P01_2022-EA-650_2022-RC-652/Models/ReservaContext.cs
+++ P01_2022-EA-650_2022-RC-652/Models/ReservaContext.cs
@@ -9,5 +9,10 @@ namespace P01_2022_EA_650_2022_RC_652.Models
             { 
                 
             }
+
+           public DbSet<Usuarios> Usuarios { get; set; }
+           public DbSet<Sucursales> Sucursales { get; set; }
+           public DbSet<Reservas> Reservas { get; set; }
+           public DbSet<EspaciosParqueo> EspaciosParqueo { get; set; }
     }
 }
