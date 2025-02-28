d5c5ccd3ae76a7304088599a6b067ab475f2275b
alex <ee653522@gmail.com>
2025-02-27 19:04:18 -0600

    CRUD de Sucursales

--- P01_2022-EA-650_2022-RC-652/Controllers/GestionEspacioParqueoController.cs
+++ P01_2022-EA-650_2022-RC-652/Controllers/GestionEspacioParqueoController.cs
@@ -20,7 +20,7 @@ namespace P01_2022_EA_650_2022_RC_652.Controllers
         [Route("MostrarSucursalesParqueo")]
         public IActionResult GetSucursales()
         { 
-            List<sucursales> listaSucursales = (from e in _reservaContext.Sucursales select e).ToList();
+            List<Sucursales> listaSucursales = (from e in _reservaContext.Sucursales select e).ToList();
             if (listaSucursales.Count == 0)
             { 
                 return NotFound();
@@ -28,5 +28,59 @@ namespace P01_2022_EA_650_2022_RC_652.Controllers
             return Ok(listaSucursales);
         }
 
+        [HttpPost]
+        [Route("CrearSucursal")]
+        public IActionResult GuardarSucursal([FromBody] Sucursales sucursales) 
+        {
+            try
+            {
+                _reservaContext.Sucursales.Add(sucursales);
+                _reservaContext.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("Modificar/{Id}")]
+        public IActionResult ModificarSucursal(int id, [FromBody] Sucursales sucursalModificar) 
+        {
+            Sucursales? sucursalActual = (from e in _reservaContext.Sucursales
+                                          where e.Id == id
+                                          select e).FirstOrDefault();
+            
+            if (sucursalActual == null) { return NotFound(); }
+
+            sucursalActual.Nombre = sucursalModificar.Nombre;
+            sucursalActual.Direccion = sucursalModificar.Direccion;
+            sucursalActual.Telefono = sucursalModificar.Telefono;
+            sucursalActual.AdministradorId = sucursalModificar.AdministradorId;
+            sucursalActual.NumeroEspacios = sucursalModificar.NumeroEspacios;
+
+            _reservaContext.Entry(sucursalActual).State = EntityState.Modified;
+            _reservaContext.SaveChanges();
+
+            return Ok(sucursalModificar);
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{Id}")]
+        public IActionResult eliminarSucursal(int id)
+        {
+            Sucursales? sucursalEliminado = (from e in _reservaContext.Sucursales
+                                             where e.Id == id
+                                             select e).FirstOrDefault();
+            if (sucursalEliminado == null) { return NotFound(); }
+            
+            _reservaContext.Sucursales.Attach(sucursalEliminado);
+            _reservaContext.Sucursales.Remove(sucursalEliminado);
+            _reservaContext.SaveChanges(true);
+
+            return Ok(sucursalEliminado);
+
+        }
     }
 }
