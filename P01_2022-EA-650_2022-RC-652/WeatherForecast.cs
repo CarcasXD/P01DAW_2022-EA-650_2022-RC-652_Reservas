1423084d448fcc737966723534a869c942712ef7
CarcasXD <carlossalesianoprro@gmail.com>
2025-02-27 18:02:17 -0600

    Agregar archivos de proyecto.

---
+++ P01_2022-EA-650_2022-RC-652/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace P01_2022_EA_650_2022_RC_652
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
