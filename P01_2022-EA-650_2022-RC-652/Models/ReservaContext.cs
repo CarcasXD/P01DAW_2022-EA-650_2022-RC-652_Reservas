8ffd9fe5e777229b0475783eae41099e9c31b984
CarcasXD <carlossalesianoprro@gmail.com>
2025-02-27 18:16:52 -0600

    Conexion e Injeccion

---
+++ P01_2022-EA-650_2022-RC-652/Models/ReservaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace P01_2022_EA_650_2022_RC_652.Models
+{
+    public class ReservaContext: DbContext
+    {
+        public ReservaContext(DbContextOptions<ReservaContext> options): base(options)
+            { 
+                
+            }
+    }
+}
