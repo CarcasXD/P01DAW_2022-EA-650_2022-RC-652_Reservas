22f088a94537d96be4fa7e8d8b194cfa6507f51b
alex <ee653522@gmail.com>
2025-02-27 18:41:18 -0600

    Empezando Controlador de Sucursales de Parqueo

---
+++ P01_2022-EA-650_2022-RC-652/Controllers/GestionEspacioParqueoController.cs
@@ -0,0 +1,32 @@
+﻿using P01_2022_EA_650_2022_RC_652.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace P01_2022_EA_650_2022_RC_652.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GestionEspacioParqueoController : ControllerBase
+    {
+        private readonly ReservaContext _reservaContext;
+
+        public GestionEspacioParqueoController(ReservaContext reservaContext)
+        {
+            _reservaContext = reservaContext;
+        }
+
+        [HttpGet]
+        [Route("MostrarSucursalesParqueo")]
+        public IActionResult GetSucursales()
+        { 
+            List<sucursales> listaSucursales = (from e in _reservaContext.Sucursales select e).ToList();
+            if (listaSucursales.Count == 0)
+            { 
+                return NotFound();
+            }
+            return Ok(listaSucursales);
+        }
+
+    }
+}
