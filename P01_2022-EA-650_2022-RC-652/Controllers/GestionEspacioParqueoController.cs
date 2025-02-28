0f7a40093d52cedb1e3827166e1b5f796dbf2f25
alex <ee653522@gmail.com>
2025-02-27 21:10:41 -0600

    ControladoresSucursales

--- P01_2022-EA-650_2022-RC-652/Controllers/GestionEspacioParqueoController.cs
+++ P01_2022-EA-650_2022-RC-652/Controllers/GestionEspacioParqueoController.cs
@@ -82,5 +82,92 @@ namespace P01_2022_EA_650_2022_RC_652.Controllers
             return Ok(sucursalEliminado);
 
         }
+
+        [HttpPost]
+        [Route("RegistrarEspacio")]
+        public IActionResult GuardarEspacio([FromBody] EspaciosParqueo espacioParqueo)
+        {
+            try
+            {
+                
+                if (_reservaContext.Sucursales.Find(espacioParqueo.SucursalId)==null)
+                {
+                    return BadRequest();
+                }
+
+                _reservaContext.EspaciosParqueo.Add(espacioParqueo);
+                _reservaContext.SaveChanges();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("DisponiblesPorDia/{fecha}")]
+        public IActionResult EspaciosDisponibles(DateTime fecha)
+        {
+            var disponibles = _reservaContext.EspaciosParqueo
+                .Where(e => e.Estado == "Disponible" &&
+                            !_reservaContext.Reservas.Any(r => r.EspacioId == e.Id && r.Fecha == fecha))
+                .ToList();
+
+            return disponibles.Any() ? Ok(disponibles) : NotFound();
+        }
+
+        [HttpPut]
+        [Route("Actualizar/{Id}")]
+        public IActionResult ActualizarEspacio(int id, [FromBody] EspaciosParqueo espacioActualizado)
+        {
+            EspaciosParqueo? espacio = (from e in _reservaContext.EspaciosParqueo
+                                        where e.Id == id
+                                        select e).FirstOrDefault();
+
+            if (espacio == null)
+                return NotFound();
+
+            espacio.Numero = espacioActualizado.Numero;
+            espacio.Ubicacion = espacioActualizado.Ubicacion;
+            espacio.CostoPorHora = espacioActualizado.CostoPorHora;
+            espacio.Estado = espacioActualizado.Estado;
+
+            _reservaContext.Entry(espacio).State = EntityState.Modified;
+            _reservaContext.SaveChanges();
+
+            return Ok(espacioActualizado);
+        }
+
+        [HttpDelete]
+        [Route("EliminarEspacio/{id}")]
+        public IActionResult EliminarEspacio(int id)
+        {
+            EspaciosParqueo? espacioEliminado = (from e in _reservaContext.EspaciosParqueo
+                                                 where e.Id == id
+                                                 select e).FirstOrDefault();
+            if (espacioEliminado == null) return NotFound();
+
+            _reservaContext.EspaciosParqueo.Attach(espacioEliminado);
+            _reservaContext.EspaciosParqueo.Remove(espacioEliminado);
+            _reservaContext.SaveChanges(true);
+
+            return Ok(espacioEliminado);
+        }
+
+        [HttpGet]
+        [Route("ReservadosPorDia/{fecha}")]
+        public IActionResult EspaciosReservadosPorDia(DateTime fecha)
+        {
+            List<Reservas> espaciosReservados = (from r in _reservaContext.Reservas
+                                                 where r.Fecha == fecha
+                                                 select r).ToList();
+
+            return espaciosReservados.Any() ? Ok(espaciosReservados) : NotFound();
+        }
+
+        
+
     }
 }
